c4fe82a4e45b1d1777cc099c0ff5c0cc46d55d16
VoidOfMaya <mayaofthevoid@gmail.com>
2023-04-09 16:12:12 +0300

    add coloring pointer path logic

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 //Etch A Sketch
 
 const mainContainer = document.querySelector('.main-container');
-let gridSize = 16 ;// prompt('write the size of the grid, max size is 100 x100');
+let gridSize = 100 ;// prompt('write the size of the grid, max size is 100 x100');
 
 
 const createGrid = function (gridSize){
@@ -9,11 +9,12 @@ const createGrid = function (gridSize){
 
     for(let x = 0; x < gridSize; x++){
         const row= document.createElement('div');
-        row.classList.add(`row-element`);           
+        row.classList.add(`row-element`); 
         mainContainer.appendChild(row);
         for(let y = 0; y < gridSize; y++){
             const column = document.createElement('div');
-            column.textContent = `${y + 1}`;
+            column.setAttribute("id",`${x},${y}`);  
+            column.textContent = "  ";
             column.classList.add('column-element');
             row.appendChild(column);
         }
@@ -22,3 +23,16 @@ const createGrid = function (gridSize){
 
 }
 createGrid(gridSize);
+
+const pixelate = document.querySelectorAll('.column-element');
+ pixelate.forEach((pixel) => pixel.addEventListener('mouseover', ()=>{
+
+    changeColor(pixel);
+    
+ }));
+
+
+ function changeColor(element){
+    element.style.backgroundColor = 'black';
+ }
+
