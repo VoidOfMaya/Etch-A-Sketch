cec7c5679f784d667f4cf7a457052f7e9ed5e537
VoidOfMaya <mayaofthevoid@gmail.com>
2023-04-09 13:22:57 +0300

    create function to generate grid element

---
+++ javascript.js
@@ -0,0 +1,18 @@
+//Etch A Sketch
+
+const mainContainer = document.querySelector('.main-container');
+let gridSize = 16 ;// prompt('write the size of the grid, max size is 100 x100');
+
+
+const createGrid = function (gridSize){
+    
+    for( let y = 0; y < gridSize; y++){
+        for(let x = 0; x < gridSize; x++){
+            const pixel= document.createElement('div');
+            pixel.classList.add(`${y},${x}`);
+            pixel.style.color = 'red';
+            mainContainer.appendChild(pixel);
+        }
+    }
+}
+createGrid(gridSize);
